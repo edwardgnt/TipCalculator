7b28c02d0ddd6a50058e7dba6df13cb020c9996f
edwardgnt <edwardgnt@gmail.com>
2019-02-05 14:41:41 -0700

    Commiting Tip Calculator

---
+++ scripts/tipCalculator.js
@@ -0,0 +1,42 @@
+// Calcualte Tip
+function calculateTip() {
+    var billAmount = document.getElementById("billAmount").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numberOfPeople = document.getElementById("peopleAmount").value;
+
+    // Validate input
+    if(billAmount === "" || serviceQuality == 0) {
+        alert("Please enter your values");
+        return ;
+    }
+
+    // Check to see if this input is empty or less than or equal to 1
+    if(numberOfPeople === "" || numberOfPeople <= 1) {
+        numberOfPeople  = 1;
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    // Calculate tip
+    var total = (billAmount * serviceQuality) / numberOfPeople;
+
+    // Round to two decimal places
+    total = Math.round(total * 100) / 100;
+
+    // Always have two digits after decimal point
+    total = total.toFixed(2);
+
+    // Display tip
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+}
+
+// Hide the tip amount when app loads
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// Click to call function
+document.getElementById("calculate").onclick = function() {
+    calculateTip();
+}
